5517812b89d3f1db27cd459246d1dbabc7b874c8
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-21 00:44:20 -0800

    Add useEffect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import { nanoid } from "nanoid";
 export default function App() {
   const [dice, setDice] = useState(allNewDice());
   const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+  }, [dice]);
+
   function generateNewDie() {
     return {
       value: Math.ceil(Math.random() * 6),
