dba872622bf5a2d33f44df5eb7e1392ad7586244
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-21 00:49:59 -0800

    Update Timer.js

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,6 +4,9 @@ export default function Timer(props) {
 
   return (
     <div className="timer">
+      <span>{("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:</span>
+      <span>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}:</span>
+      <span>{("0" + ((props.time / 10) % 100)).slice(-2)}</span>
     </div>
   );
 }
