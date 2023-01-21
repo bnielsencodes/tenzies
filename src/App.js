c2d8317354b32c9555e340e68c52604757ba2b14
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-21 00:08:14 -0800

    Add title and instructions

--- src/App.js
+++ src/App.js
@@ -3,5 +3,12 @@ import React, { useState, useEffect } from "react";
 export default function App() {
 
   return (
+    <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+    </main>
   );
 }
