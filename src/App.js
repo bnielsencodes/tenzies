d991da7ddd8f318cead6c646e5321522589a6d59
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-21 00:38:08 -0800

    Create holdDice function

--- src/App.js
+++ src/App.js
@@ -37,6 +37,14 @@ export default function App() {
     }
   }
 
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
   const diceElements = dice.map((die) => (
     <Die
       key={die.id}
