e5b324dbba6bba6732a675132f164a70dbacb100
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-21 00:41:40 -0800

    Add styles to dice

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
   return (
-    <div className="die-face">
-      <h2 className="die-num"></h2>
+    <div className="die-face" style={styles}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
