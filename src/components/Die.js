6124c9277b0ea14d508fa2a61c7966421bde8be7
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-28 19:53:52 -0800

    Import dice face svgs

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,4 +1,9 @@
-import React from "react";
+import diceOne from "../assets/dice-one-regular.svg";
+import diceTwo from "../assets/dice-two-regular.svg";
+import diceThree from "../assets/dice-three-regular.svg";
+import diceFour from "../assets/dice-four-regular.svg";
+import diceFive from "../assets/dice-five-regular.svg";
+import diceSix from "../assets/dice-six-regular.svg";
 
 export default function Die(props) {
   const styles = {
