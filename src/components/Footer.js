dc22ba5fa35466099b66a08b29a429ba7ab588de
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-01-28 19:52:31 -0800

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer(props) {
+  return (
+    <p className="footer">
+      Made by{" "}
+      <a href="https://bnielsen.dev" target="_blank" rel="noreferrer">
+        Brandon Nielsen
+      </a>
+    </p>
+  );
+}
